2298dc68992211caf0478402124dc8aabb102b9e
Edgar Jurkoit <90857989+edgaros24@users.noreply.github.com>
2023-10-14 20:21:50 +0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    fontSize: 32,
+    padding: 40,
+    backgroundColor: 'black',
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 20,
+    marginBottom: 40,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  playerButton: {
+    padding: 10,
+    backgroundColor: 'lightblue',
+    borderWidth: 1,
+    borderColor: 'lightblue',
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  playerButtonText: {
+    color: 'black',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+  name: {
+    fontSize: 28,
+    textAlign: 'center',
+    marginVertical: 15,
+  },
+  car: {
+    fontSize: 28,
+    textAlign: 'center',
+    marginVertical: 15,
+  },
+  sectionTitle: {
+    fontSize: 28,
+    marginVertical: 15,
+  },
+  raceItem: {
+    fontSize: 28,
+    marginVertical: 15,
+  },
+});
